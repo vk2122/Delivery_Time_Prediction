3230cd9bb743b1083959f4d4c940240ddfb608a8
Varun Khachane <varunkhachane31@gmail.com>
2023-05-22 02:21:32 +0530

    ui/app design updated

--- delivery_time_predictor/lib/main.dart
+++ delivery_time_predictor/lib/main.dart
@@ -1,115 +1,274 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:flutter_xlider/flutter_xlider.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Delivery Time Prediction',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        useMaterial3: true,
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: DeliveryPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class DeliveryPage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _DeliveryPageState createState() => _DeliveryPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _DeliveryPageState extends State<DeliveryPage> {
+  final TextEditingController _deliveryPersonRatingController =
+      TextEditingController();
+  String? _weatherCondition;
+  String? _roadTrafficDensity;
+  String? _vehicleCondition;
+  String? _multipleDeliveries;
+  double? _distance;
+  double? _deliveryPersonAge;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _submitPrediction() async {
+    final response = await http.get(Uri.parse('http://127.0.0.1:5000/predict'));
+
+    if (response.statusCode == 200) {
+      final responseData = json.decode(response.body);
+      final prediction = responseData['predictions'][0];
+
+      showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(
+            height: 200,
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Predicted Delivery Time',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                  maxLines: 1,
+                ),
+                SizedBox(height: 10.0),
+                Text(
+                  '$prediction minutes',
+                  style: TextStyle(
+                    fontSize: 26.0,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          );
+        },
+      );
+    } else {
+      print('API call failed with status code: ${response.statusCode}');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Delivery Time Prediction'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Text(
+                'Delivery Person Details',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              FlutterSlider(
+                values: [_deliveryPersonAge ?? 18],
+                min: 18,
+                max: 55,
+                onDragging: (handlerIndex, lowerValue, upperValue) {
+                  setState(() {
+                    _deliveryPersonAge = lowerValue;
+                  });
+                },
+                tooltip: FlutterSliderTooltip(
+                  format: (value) {
+                    return value.toString();
+                  },
+                  positionOffset: FlutterSliderTooltipPositionOffset(
+                    top: -40,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                'Age: ${_deliveryPersonAge?.toStringAsFixed(0) ?? ''}',
+                style: TextStyle(fontSize: 16.0),
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                'Delivery Person Rating',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              TextFormField(
+                controller: _deliveryPersonRatingController,
+                decoration:
+                    InputDecoration(labelText: 'Delivery Person Rating'),
+                keyboardType: TextInputType.number,
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                'Weather Conditions',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              DropdownButtonFormField<String>(
+                value: _weatherCondition,
+                decoration: InputDecoration(labelText: 'Weather Conditions'),
+                onChanged: (value) {
+                  setState(() {
+                    _weatherCondition = value!;
+                  });
+                },
+                items: ['Sunny', 'Cloudy']
+                    .map((value) => DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        ))
+                    .toList(),
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                'Road Traffic Density',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              DropdownButtonFormField<String>(
+                value: _roadTrafficDensity,
+                decoration: InputDecoration(labelText: 'Road Traffic Density'),
+                onChanged: (value) {
+                  setState(() {
+                    _roadTrafficDensity = value!;
+                  });
+                },
+                items: ['High', 'Medium', 'Low']
+                    .map((value) => DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        ))
+                    .toList(),
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                'Vehicle Condition',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              DropdownButtonFormField<String>(
+                value: _vehicleCondition,
+                decoration: InputDecoration(labelText: 'Vehicle Condition'),
+                onChanged: (value) {
+                  setState(() {
+                    _vehicleCondition = value!;
+                  });
+                },
+                items: ['0', '1', '2']
+                    .map((value) => DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        ))
+                    .toList(),
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                'Multiple Deliveries',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              DropdownButtonFormField<String>(
+                value: _multipleDeliveries,
+                decoration: InputDecoration(labelText: 'Multiple Deliveries'),
+                onChanged: (value) {
+                  setState(() {
+                    _multipleDeliveries = value!;
+                  });
+                },
+                items: ['Yes', 'No']
+                    .map((value) => DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        ))
+                    .toList(),
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                'Distance',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              FlutterSlider(
+                values: [_distance ?? 0],
+                min: 0,
+                max: 10,
+                onDragging: (handlerIndex, lowerValue, upperValue) {
+                  setState(() {
+                    _distance = lowerValue;
+                  });
+                },
+                tooltip: FlutterSliderTooltip(
+                  format: (value) {
+                    return value.toString();
+                  },
+                  positionOffset: FlutterSliderTooltipPositionOffset(
+                    top: -40,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                'Distance: ${_distance?.toStringAsFixed(0) ?? ''} km',
+                style: TextStyle(fontSize: 16.0),
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: _submitPrediction,
+                child: Text('Submit'),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
