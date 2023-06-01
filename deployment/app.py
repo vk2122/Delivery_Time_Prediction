b661323fc7704a52733dd641d335b2938c6343cf
Varun Khachane <varunkhachane31@gmail.com>
2023-06-01 22:36:36 +0530

    flask updated

--- deployment/app.py
+++ deployment/app.py
@@ -5,7 +5,6 @@ import xgboost as xgb
 
 app = Flask(__name__)
 
-# Load the saved model from the pickle file
 filename = 'D:/Delivery_Time_Prediction/model/timePred.pkl'
 with open(filename, 'rb') as file:
     loaded_model = pickle.load(file)
@@ -15,13 +14,10 @@ with open(filename, 'rb') as file:
 def predict():
     data = request.get_json()
 
-    # Create a DataFrame from the input data
     input_df = pd.DataFrame(data)
 
-    # Make predictions using the loaded model and input data
     predictions = loaded_model.predict(input_df)
 
-    # Return the predictions as a JSON response
     response = {'predictions': predictions.tolist()}
     return jsonify(response)
 
